cce57e3c20d1066d4e35d4ede83773552af1f816
elips0675 <elips0675@gmail.com>
2026-04-10 01:29:28 +0000

    Добавлена главная страница

---
+++ index.php
@@ -0,0 +1,15 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Главная");
+?>
+
+<div class="container py-5">
+    <div class="jumbotron">
+        <h1 class="display-4">Добро пожаловать!</h1>
+        <p class="lead">Это главная страница сайта, созданного на основе React-проекта.</p>
+        <hr class="my-4">
+        <p>Используйте меню для навигации по сайту.</p>
+    </div>
+</div>
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
