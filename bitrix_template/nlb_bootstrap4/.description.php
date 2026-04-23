4d5ec70e22f92afa723172c3fa01a7a38c3c5875
elips0675 <elips0675@gmail.com>
2026-04-23 19:35:49 +0000

    мое сообщение о коммите

--- bitrix_template/nlb_bootstrap4/.description.php
+++
@@ -1,8 +0,0 @@
-<?
-if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
-
-$arTemplate = array(
-    'NAME' => 'NLB Bootstrap 4',
-    'DESCRIPTION' => 'Стартовый шаблон на Bootstrap 4 для NLB Studio'
-);
-?>
